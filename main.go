b5272e55e391cdfcd7ea4e42fa97be51f9b62ea5
Alex Vargas <alexvargas561@gmail.com>
2024-06-29 21:07:17 -0700

    learning and better understanding of golang

--- main.go
+++ main.go
@@ -5,12 +5,21 @@ import (
 	"net/http"
 )
 
+func HandleHealthCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	s := "OK"
+	b := []byte(s)
+	w.Write(b)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+}
+
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.HandleFunc("/healthz", HandleHealthCheck)
+	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
