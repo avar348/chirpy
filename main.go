7dc5c1bf78b1c8ddbabba6d1f8a9cfefeec9531f
Alex Vargas <alexvargas561@gmail.com>
2024-06-29 21:19:47 -0700

    made changes to the read document

--- main.go
+++ main.go
@@ -6,11 +6,9 @@ import (
 )
 
 func HandleHealthCheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	s := "OK"
-	b := []byte(s)
-	w.Write(b)
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 
 func main() {
