95cfbcf40e88b9141bf59ebe67335532195cd3b6
Alex Vargas <alexvargas561@gmail.com>
2024-08-07 10:52:37 -0700

    made the fix for the create

--- handlechirpget.go
+++ handlechirpget.go
@@ -16,8 +16,9 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
 	chirps := []Chirp{}
 	for _, dbChirp := range dbChirps {
 		chirps = append(chirps, Chirp{
-			ID:   dbChirp.ID,
-			Body: dbChirp.Body,
+			ID:       dbChirp.ID,
+			Body:     dbChirp.Body,
+			AuthorID: dbChirp.AuthorID,
 		})
 	}
 
@@ -43,7 +44,8 @@ func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
 	}
 
 	respondWithJSON(w, http.StatusOK, Chirp{
-		ID:   dbChirp.ID,
-		Body: dbChirp.Body,
+		ID:       dbChirp.ID,
+		Body:     dbChirp.Body,
+		AuthorID: dbChirp.AuthorID,
 	})
 }
